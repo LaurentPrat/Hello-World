9f75a247bff9c2f2b441621d2ed3aafd6806d6d6
laurent Prat <laurent.prat@mailwatcher.net>
2020-09-11 09:00:26 +0200

    more code for testing

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 /*
+
 https://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
+https://github.com/richelbilderbeek/travis_cpp_tutorial
+
 */
 
+// std
 #include <time.h>
 #include <stdio.h>
- 
+
+// for getwebpage
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+// local
+int getwebpage();
+
 int main(void)
 {
     struct tm *newtime;
@@ -16,5 +29,57 @@ int main(void)
     newtime = localtime(&ltime);
  
         /* Print the local time as a string */
-    printf("Hello, the current date and time are %s", asctime(newtime));
+    printf("Hello, the current date and time are %s\n", asctime(newtime));
+    getwebpage();
+}
+
+
+
+int getwebpage()
+{
+    /*Define socket variables */
+    char host[1024] = "pastebin.com";
+    char url[1024] = "/raw/7y7MWssc";
+    char request[2000];
+    struct hostent *server;
+    struct sockaddr_in serverAddr;
+    int portno = 80;
+
+    printf("Trying to get source of pastebin.com/raw/7y7MWssc ...\n");
+
+    /* Create socket */
+    int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(tcpSocket < 0) 
+    {
+        printf("ERROR opening socket\n");
+    } else 
+    {
+        printf("Socket opened successfully.\n");
+    }
+
+    server = gethostbyname(host);
+    serverAddr.sin_port = htons(portno);
+    serverAddr.sin_family = AF_INET;
+
+    if(connect(tcpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) 
+    {
+        printf("Can't connect\n");
+    } else 
+    {
+        printf("Connected successfully\n");
+    }
+
+    bzero(request, 2000);
+    sprintf(request, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", url, host);
+    printf("\n%s", request);
+
+    if(send(tcpSocket, request, strlen(request), 0) < 0) {
+        printf("Error with send()");
+        return(1);
+    } else {
+        printf("Successfully sent html fetch request");
+        return(0);
+    }
+    printf("test\n");
+
 }
